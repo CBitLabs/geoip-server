2486ea3051e64e01a8138085e31b69d5949667a0
Joshua Blum <jblum@bitsighttech.com>
2014-01-08 13:44:02 -0500

    server skeleton

---
+++ reporting-server/server.py
@@ -0,0 +1,35 @@
+"""
+    Simple server to accept geo-ip requests.
+"""
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/geo-ip")
+def geo_ip():
+    """
+        Endpoint to process a geo-ip request. 
+    """
+    
+    res = {
+        'success': True,
+        'lat' : request.args.get('lat'),
+        'lng' : request.args.get('lng'),
+        'mac_addr' : request.args.get('mac_addr'),
+        'dev_id' : request.args.get('dev_id'),
+        'ip' : request.args.remote_addr,
+    }
+
+    if not all(res.values()):
+        res['success'] = False
+    
+    return jsonify(**res)
+
+
+if __name__ == "__main__":
+    DEBUG = True
+    HOST = "127.0.0.1"
+    PORT = 8000
+    app.debug = DEBUG
+    app.run(host=HOST, port=PORT)
