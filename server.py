302704a116ec106c2083274f4cbcf82b808a3c6c
Joshua Blum <jblum@bitsighttech.com>
2014-01-09 17:35:05 -0500

    db boilerplate

--- server.py
+++ server.py
@@ -1,10 +1,31 @@
 """
     Simple server to accept geo-ip requests.
 """
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+class GeoIP(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lat = db.Column(db.String(80))
+    lng = db.Column(db.String(80))
+    bssid = db.Column(db.String(80))
+    ssid = db.Column(db.String(80))
+    uuid = db.Column(db.String(80))
+    ip = db.Column(db.String(80))
+
+    def __init__(self, **kwargs):
+        self.lat = kwargs['lat']
+        self.lng = kwargs['lng']
+        self.bssid = kwargs['bssid']
+        self.ssid = kwargs['ssid']
+        self.uuid = kwargs['uuid']
+        self.ip = kwargs['ip']
 
 @app.route("/geoip", methods=["GET", "POST"])
 def geoip():
@@ -29,6 +50,10 @@ def geoip():
 
     if not all(res.values()):
         res['success'] = False
+    else:
+        geoip_obj = GeoIP(**res)
+        db.session.add(geoip_obj)
+        db.session.commit()
     
     return jsonify(**res)
 
