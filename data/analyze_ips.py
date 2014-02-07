d8ce9209fe1abcf64eb45cb2f88680357169801d
Joshua Blum <joshblum@mit.edu>
2014-02-07 11:34:06 -0500

    subnet, ip, total stats

--- data/analyze_ips.py
+++ data/analyze_ips.py
@@ -3,6 +3,9 @@
     to find data to contruct pipeline
 """
 
+from optparse import OptionParser
+
+import matplotlib.pyplot as plt
 import psycopg2 as pg
 import numpy as np
 import os
@@ -24,32 +27,37 @@ EVENTS = [
     "unexp_freq",
 ]
 
-QUERY = """SELECT COALESCE(SUM(spam_count), 0), 
+COLORS = ['b', 'g', 'r', 'c', 'm', 'y']
+
+QUERY = """SELECT COALESCE(SUM(spam_count), 0),
                 COALESCE(SUM(spam_freq), 0), 
                 COALESCE(SUM(bot_count), 0), 
                 COALESCE(SUM(bot_freq), 0), 
                 COALESCE(SUM(unexp_count), 0), 
                 COALESCE(SUM(unexp_freq), 0) 
-            FROM ip_events WHERE address%(op)sINET '%(ip)s';"""
+            FROM ip_events_%(days)d WHERE address%(op)sINET '%(ip)s';"""
 
 IN_FILE = "ip_list.csv"
 
 
-def main():
+def main(days, useSubnet):
 
     print "\nLoading ips..."
     ips = load_ips()
 
     print "\nBuilding stats..."
-    ip_stats, subnet_stats = get_stats(ips)
+    ip_stats, subnet_stats = get_stats(ips, days, useSubnet)
 
     print "\nPrinting IP stats..."
     print_stats(ip_stats)
 
-    print "="*60
+    if useSubnet:
+        print "=" * 60
 
-    print "\nPrinting subnet stats..."
-    print_stats(subnet_stats)
+        print "\nPrinting subnet stats..."
+        print_stats(subnet_stats)
+
+    plt.show()
 
 
 def load_ips(in_file=IN_FILE):
@@ -58,18 +66,25 @@ def load_ips(in_file=IN_FILE):
         return [row[0] for row in reader]
 
 
-def get_stats(ips):
+def get_stats(ips, days, useSubnet):
     """walk through list of ip addresses and gather sum across events"""
     cur = get_cursor()
     ip_stats = {}
     subnet_stats = {}
     for i, ip in enumerate(ips):
-        ip_stats[ip] = sum_query(cur, ip, "=")
-        subnet = get_subnet(ip)
-        subnet_stats[subnet] = sum_query(cur, subnet, "<<")
+        ip_stats[ip] = sum_query(cur, ip, "=", days)
+
+        if useSubnet:
+            subnet = get_subnet(ip)
+            subnet_stats[subnet] = sum_query(cur, subnet, "<<", days)
 
         if not i % 100:
             print "Queried %d rows." % i
+
+    plot_stats("IP stats", ip_stats)
+
+    if useSubnet:
+        plot_stats("subnet stats", subnet_stats)
 
     return ip_stats, subnet_stats
 
@@ -81,8 +96,8 @@ def get_cursor(auth=AUTH):
     return conn.cursor()
 
 
-def sum_query(cur, ip, op):
-    query = make_query(ip, op)
+def sum_query(cur, ip, op, days):
+    query = make_query(ip, op, days)
     cur.execute(query)
 
     rows = cur.fetchall()
@@ -90,20 +105,23 @@ def sum_query(cur, ip, op):
     return as_dict(rows[0])
 
 
-def make_query(ip, op):
+def make_query(ip, op, days):
     return QUERY % {
         'ip': ip,
-        'op': op
+        'op': op,
+        'days': days
     }
 
 
 def as_dict(row):
     return {k: v for k, v in zip(EVENTS, row)}
 
+
 def get_subnet(ip):
     return ".".join(ip.split(".")[:-1]) + "/24"
 
-def print_stats(ip_stats):
+
+def print_stats(stats):
     """
         dictionary of form {
             'ip' : {
@@ -114,42 +132,122 @@ def print_stats(ip_stats):
     print the min, max, avg type of each event for each ip
     print the number of ips with no events
     """
+    print "Found %d events" % len(stats)
+
+    # get event stats
     for event in EVENTS:
-        print_int_stat("max", event, *get_max(ip_stats, event))
-        print_int_stat("min", event, *get_min(ip_stats, event))
-        print_float_stat("avg", event, get_avg(ip_stats, event))
-        print_float_stat("stdev", event, get_stdev(ip_stats, event))
+        vals = get_vals(stats, event)
+        print_dict_stat("max", event, *get_max(stats, event))
+        print_dict_stat("min", event, *get_min(stats, event))
+        print_float_stat("avg", event, get_avg(vals))
+        print_float_stat("stdev", event, get_stdev(vals))
+        print_percent_stat("zeros", event, get_zero_percent(vals))
         print
 
+    print "-" * 60 + "\n"
+    # get tot stats
+    print_tot_stats("freq", sum_freq(stats))
+    print_tot_stats("count", sum_count(stats))
 
-def print_int_stat(stat, event, ip, stats_dict):
+
+def print_tot_stats(name, stats):
+    print_int_stat("max", name, max(stats))
+    print_int_stat("min", name, min(stats))
+    print_float_stat("avg", name, get_avg(stats))
+    print_float_stat("stdev", name, get_stdev(stats))
+    print_percent_stat("zeros", name, get_zero_percent(stats))
+    print
+
+
+def print_dict_stat(stat, event, ip, stats_dict):
 
     val = stats_dict[event]
     print "%s of %s from %s is %d." % (stat, event, ip, val)
 
 
+def print_int_stat(stat, event, val):
+    _print_stat(stat, event, "%d", val)
+
+
 def print_float_stat(stat, event, val):
-    print "%s of %s is %03f." % (stat, event, val)
+    _print_stat(stat, event, "%3f", val)
 
 
-def get_max(ip_stats, event):
-    return max(ip_stats.iteritems(), key=lambda kv: kv[1][event])
+def print_percent_stat(stat, event, val):
+    _print_stat(stat, event, "%.2f%%", val)
 
 
-def get_min(ip_stats, event):
-    return min(ip_stats.iteritems(), key=lambda kv: kv[1][event])
+def _print_stat(stat, event, fmt, val):
+    string = "%s of %s is %s." % (stat, event, fmt)
+    print string % val
 
 
-def get_avg(ip_stats, event):
-    return sum(
-        map(
-            lambda kv: kv[1][event], ip_stats.iteritems())) / float(len(ip_stats))
+def get_max(stats, event):
+    return max(stats.iteritems(), key=lambda kv: kv[1][event])
 
 
-def get_stdev(ip_stats, event):
-    vals = map(lambda kv: kv[1][event], ip_stats.iteritems())
-    return np.std(vals)
+def get_min(stats, event):
+    return min(stats.iteritems(), key=lambda kv: kv[1][event])
 
+
+def get_avg(stats):
+    return np.average(stats)
+
+
+def get_stdev(stats):
+    return np.std(stats)
+
+
+def get_zero_percent(stats):
+    num_zeros = len(filter(lambda x: x == 0, stats))
+    return round(num_zeros / float(len(stats)), 2)
+
+
+def plot_stats(title, stats):
+    plt.figure()
+    bins = len(stats) / 100
+    plt.hist(sum_count(stats), bins=bins,
+             histtype='stepfilled',
+             color='r', label="count")
+
+    plt.hist(sum_freq(stats), bins=bins,
+             histtype='stepfilled',
+             color='b', label="freq")
+
+    plt.title(title)
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.draw()
+
+
+def sum_count(stats):
+    return sum_stats(get_to_zip(stats, "freq"))
+
+
+def sum_freq(stats):
+    return sum_stats(get_to_zip(stats, "count"))
+
+
+def get_to_zip(stats, keyword):
+    return [get_vals(stats, e) for e in EVENTS if keyword in e]
+
+
+def sum_stats(to_zip, ):
+    return map(sum, zip(*to_zip))
+
+
+def get_vals(stats, event):
+    return map(lambda kv: kv[1][event], stats.iteritems())
 
 if __name__ == "__main__":
-    main()
+
+    parser = OptionParser()
+    parser.add_option("-d", "--days", default=50, dest="days",
+                      help="Number of days, either 50 or 100. Default 50.")
+    parser.add_option("-s", "--subnet",
+                      action="store_false", dest="useSubnet", default=True,
+                      help="compute subnet stats")
+
+    (options, args) = parser.parse_args()
+    main(options.days, options.useSubnet)
--- data/ip_events.sql
+++ data/ip_events.sql
@@ -1,4 +1,4 @@
-CREATE TABLE ip_events (
+CREATE TABLE ip_events_100 (
     date int,
     address inet,
     spam_count int,
@@ -8,5 +8,5 @@ CREATE TABLE ip_events (
     unexp_count int,
     unexp_freq int);
 
-\copy ip_events from '/Users/stuart/Desktop/ip-stats-50.txt' DELIMITER ',' CSV;
-CREATE INDEX addr_idx on ip_events (address);
+\copy ip_events_100 from '/Users/jblum/work/ip-stats-16000-16099.csv' DELIMITER ',' CSV;
+CREATE INDEX addr_idx on ip_events_100 (address);
