ddc3f9343906a115ac9609e7d75a772cf03f02d8
Joshua Blum <joshblum@mit.edu>
2014-02-07 13:07:13 -0500

    indx

--- data/ip_events.sql
+++ data/ip_events.sql
@@ -9,4 +9,4 @@ CREATE TABLE ip_events_100 (
     unexp_freq int);
 
 \copy ip_events_100 from '/Users/jblum/work/ip-stats-16000-16099.csv' DELIMITER ',' CSV;
-CREATE INDEX addr_idx on ip_events_100 (address);
+CREATE INDEX addr_100_idx on ip_events_100 (address);
